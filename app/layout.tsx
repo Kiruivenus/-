39b6723e55d19a5f0c0728c009e877133974c0da
Kiruivenus <139698673+Kiruivenus@users.noreply.github.com>
2026-03-04 08:06:23 +0300

    Update metadata and layout styles in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,25 +4,57 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'EMCD All things Crypto in one App',
-  description: 'An ecosystem for mining and working with cryptocurrencies powered by one of the world top 7 mining pools. ',
+  metadataBase: new URL('https://m057-yv-uf-msgw-phttd-dxfx.vercel.app'),
+
+  title: 'EMCD All Things Crypto in One App',
+  description:
+    "An ecosystem for mining and working with cryptocurrencies powered by one of the world's top 7 mining pools.",
   generator: 'venus',
+
+  openGraph: {
+    type: 'website',
+    url: 'https://m057-yv-uf-msgw-phttd-dxfx.vercel.app',
+    title: 'EMCD All Things Crypto in One App',
+    description:
+      "An ecosystem for mining and working with cryptocurrencies powered by one of the world's top 7 mining pools.",
+    images: [
+      {
+        url: 'https://i.im.ge/2026/03/04/eYVdSL.969883.jpeg',
+        width: 1200,
+        height: 630,
+        alt: 'EMCD Crypto App Preview',
+      },
+    ],
+  },
+
+  twitter: {
+    card: 'summary_large_image',
+    title: 'EMCD All Things Crypto in One App',
+    description:
+      "An ecosystem for mining and working with cryptocurrencies powered by one of the world's top 7 mining pools.",
+    images: ['https://i.im.ge/2026/03/04/eYVdSL.969883.jpeg'],
+  },
+
+  icons: {
+    icon: 'https://i.im.ge/2026/03/04/eYVsVx.969902.jpeg',
+    apple: 'https://i.im.ge/2026/03/04/eYVsVx.969902.jpeg',
+  },
 }
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
     <html lang="en">
       <head>
         <style>{`
-html {
-  font-family: ${GeistSans.style.fontFamily};
-  --font-sans: ${GeistSans.variable};
-  --font-mono: ${GeistMono.variable};
-}
+          html {
+            font-family: ${GeistSans.style.fontFamily};
+            --font-sans: ${GeistSans.variable};
+            --font-mono: ${GeistMono.variable};
+          }
         `}</style>
       </head>
       <body>{children}</body>
