a0b60ce5cea3a6f29d55c64a3091175ab5bd7189
Kiruivenus <139698673+Kiruivenus@users.noreply.github.com>
2026-03-04 07:44:20 +0300

    Change app metadata for new branding
    Updated metadata for the application to reflect new title, description, and generator.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'EMCD All things Crypto in one App',
+  description: 'An ecosystem for mining and working with cryptocurrencies powered by one of the world top 7 mining pools. ',
+  generator: 'venus',
 }
 
 export default function RootLayout({
